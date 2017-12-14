e2ab42cf05d79c7f6e80750307c1bd99d6db20cc
phaka8548 <34377705+phaka8548@users.noreply.github.com>
2017-12-14 13:22:07 -0600

    Refactoring class

--- DVDGroupProject/src/CheckedOut.java
+++ DVDGroupProject/src/CheckedOut.java
@@ -1,54 +1,40 @@
-public class CheckedOut {
+public class CheckedOut {			//object that holds an account number and the DVD's associated with that account #
 	
-	private double accountNumber;
-	private DvdType[] DVDs = new DvdType[5];
-	String[] titles = new String[5];
-	
-	public CheckedOut(double accountNumber, DvdType[] DVDs) {
-		super();
-		this.accountNumber = accountNumber;
-		this.DVDs = DVDs;
-	}
-	
-	
-	public CheckedOut() {}
 
+	//checked out list
+	//rent thats going to search through DVD list and reduce its number of copies by one
+	//and find the customer in the customer list and adds a DVD to their list
+	//and in the checkout positional list - grab the DVD that was checked and the account # that holds it
+	//before adding object to a list, search through list, find ID number, and add that DVD to that ID
+	//if they don't have anything checked out, create a new checked out object and add that to that array
+	//need a 3rd search method to search through CheckedOut positional list
+	
+	
+	private double accountNumber;				//holding account number of a person
+	private DvdType[] DVDs = new DvdType[5];	//holding DVDs person checked out (rented)
+	
 
-	public double getAccountNumber() {
-		return accountNumber;
-	}
-	public void setAccountNumber(double accountNumber) {
-		this.accountNumber = accountNumber;
-	}
-	public String[] getTitles() {
-		for (int i = 0; i < DVDs.length ; i++)	//can only fill titles based on the number of DVDs 
-		{
-			
-			titles[i] =  DVDs[i].getTitle();		//fill array with titles of DVD's in list
-			
-		}
-		return titles;
-	}
-	public void setTitles(DvdType[] DVDs)
+	//checkout - add a DVD to the customer's list and decrement the DVDs copies by 1
+	//in order to implement, create a customer, and a DVD
+	/**
+	 * 
+	 * @param CustomerType person who is checking out DVD
+	 * @param outDVD the DVD the person is checking out
+	 */
+	public void checkOut(CustomerType person, DvdType outDVD)
 	{
-		for (int i = 0; i < DVDs.length ; i++)	//can only fill titles based on the number of DVDs 
-		{
-			if (DVDs[i] != null)				//to handle null pointer exceptions
-			titles[i] = DVDs[i].getTitle();		//fill array with titles of DVD's in list
-			else
-				continue;						//keep going through for loop
-		}
-		
-	}
-
-
-	@Override
-	public String toString() {
-		return "CheckedOut [accountNumber=" + accountNumber + ", title=" + titles + "]";
+		person.rentDVD(outDVD);					//add that DVD to the person's list
+		outDVD.setCopies(outDVD.getCopies()-1);	//decrement the number of copies by 1
 	}
 	
-
-
-	
-	
+	/**
+	 * These methods will be used within the class
+	 * @param person
+	 * @param inDVD
+	 */
+	public void checkIn(CustomerType person, DvdType inDVD)
+	{
+		person.returnDVD(inDVD);				//person basically returns DVD
+		inDVD.setCopies(inDVD.getCopies()+1);	//increment the number of copies by 1
+	}
 }
